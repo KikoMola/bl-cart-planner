64078df697c9aca97e3f1eeff2faff7d33acc7d1
Kiko <kikoguirado@gmail.com>
2025-11-05 20:27:12 +0100

    feat(table): add table component with routing and basic template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Table } from './pages/table/table';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home,
+    },
+    {
+        path: 'table',
+        component: Table,
     },
 ];
